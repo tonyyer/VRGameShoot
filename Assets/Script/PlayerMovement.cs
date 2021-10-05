8627ac2f428c0cbdd2bca422522ddf6c0b3b208c
anthony flor velasquez <antony9015@gmail.com>
2021-10-04 19:11:40 -0500

    inicio de proyecto

---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 2.5f;
+    public Transform cameraPlayer;
+    public float angleLin = 30;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float angle = cameraPlayer.rotation.eulerAngles.x;
+        if(angle<angleLin){
+            GetComponent<Rigidbody>().velocity = cameraPlayer.forward*speed;
+        }else{
+            GetComponent<Rigidbody>().velocity = new Vector3(0,0,0);
+        }
+    }
+}

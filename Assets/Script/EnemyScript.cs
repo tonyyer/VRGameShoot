79090e44713b35835feee38ce6b57ee7018ffbf3
anthony flor velasquez <antony9015@gmail.com>
2021-10-13 20:29:42 -0500

    score e iluminacion

--- Assets/Script/EnemyScript.cs
+++ Assets/Script/EnemyScript.cs
@@ -1,15 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.AI;
 public class EnemyScript : MonoBehaviour
 {
     public NavMeshAgent NVagent;
     public Vector3 randomPosition;
     float elapsed;
+
+    Text scoreTxt;
     void Start()
     {
         elapsed = 1000;
+
+        scoreTxt = GameObject.Find("score").GetComponent<Text>();
     }
 
     // Update is called once per frame
@@ -22,5 +27,14 @@ public class EnemyScript : MonoBehaviour
             elapsed = 0;
         }
 
+    }
+
+        private void OnCollisionEnter(Collision collision){
+        if(collision.gameObject.tag=="bullet"){
+            Destroy(collision.gameObject,0.1f);
+            Destroy(gameObject,0.1f);
+
+            scoreTxt.text = ""+ (int.Parse(scoreTxt.text)+1);
+        }
     }
 }
